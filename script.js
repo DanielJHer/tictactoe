5b0185f295cd567986fbba67db187373af65f30c
Daniel Her <danieljhher@gmail.com>
2022-11-16 23:16:38 -0800

    first push

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const mainGrid = document.querySelector(".main-grid");
+
+const gameboard = () => {
+  array = [];
+  renderArray = (array) => {};
+  return { array };
+};
+
+const players = () => {
+  return {};
+};
